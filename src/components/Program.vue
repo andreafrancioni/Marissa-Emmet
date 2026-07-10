5f8c6488482365a712b26013550b066084328503
andrea.francioni <andrea.francioni@dedagroup.it>
2026-07-10 16:21:43 +0200

    fix: update attire description from Classical Formal to Classic Formal in Program component

--- src/components/Program.vue
+++ src/components/Program.vue
@@ -36,7 +36,7 @@ const days = [
         locationUrl:
           "https://www.google.com/maps/search/?api=1&query=Chiesa+dei+Santi+Michele+e+Gaetano+Florence",
         description:
-          "We are honored to celebrate our marriage at the beautiful Church of Saints Michael and Gaetano, one of Florence's most treasured Baroque churches.\n\nFollowing the ceremony, guests are invited to continue the celebrations at Villa Mangiacane in the heart of the Tuscan countryside.\n\nAttire: Classical Formal\n\nTransportation will be provided from the church to Villa Mangiacane and, at the conclusion of the evening, from Villa Mangiacane back to guests' accommodations.",
+          "We are honored to celebrate our marriage at the beautiful Church of Saints Michael and Gaetano, one of Florence's most treasured Baroque churches.\n\nFollowing the ceremony, guests are invited to continue the celebrations at Villa Mangiacane in the heart of the Tuscan countryside.\n\nAttire: Classic Formal\n\nTransportation will be provided from the church to Villa Mangiacane and, at the conclusion of the evening, from Villa Mangiacane back to guests' accommodations.",
       },
       {
         title: "Wedding Reception & After-Party",
